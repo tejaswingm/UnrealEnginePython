984254c22b790576ae7464e8e75fe826b4a766af
getnamo <getnamo@gmail.com>
2017-05-25 05:49:38 +0100

    make finding module logs a bit easier

--- Content/Scripts/upymodule_importer.py
+++ Content/Scripts/upymodule_importer.py
@@ -19,24 +19,24 @@ def parseJson(packagePath):
 		with open(packagePath) as data_file:
 			#TODO: catch file not found error    
 			package = json.load(data_file)
-			ue.log('Resolving upymodule dependencies for ' + package['name'])
+			ue.log('upymodule_importer::Resolving upymodule dependencies for ' + package['name'])
 			pythonModules = package['pythonModules']
 
 			#loop over all the modules, check if we have them installed
 			for module in pythonModules:
 				version = pythonModules[module]
 
-				ue.log(module + " " + version + " installed? " + str(pip.isInstalled(module)))
+				ue.log("upymodule_importer::" + module + " " + version + " installed? " + str(pip.isInstalled(module)))
 				if not pip.isInstalled(module, version):
-					ue.log('Dependency not installed, fetching via pip...')
+					ue.log('upymodule_importer::Dependency not installed, fetching via pip...')
 					pip.install(module + '==' + version)
 
 			dependencyNoun = 'dependencies'
 			if len(pythonModules) == 1:
 				dependencyNoun = 'dependency'
 
-			ue.log(str(len(pythonModules)) + ' ' + package['name'] + ' upymodule ' + dependencyNoun + ' resolved (if installation in progress, more async output will stream)')
+			ue.log('upymodule_importer::' + str(len(pythonModules)) + ' ' + package['name'] + ' upymodule ' + dependencyNoun + ' resolved (if installation in progress, more async output will stream)')
 	except:
 		e = sys.exc_info()[0]
-		ue.log('upymodule.json error: ' + str(e))
+		ue.log('upymodule_importer::upymodule.json error: ' + str(e))
 
